7c590e10c8939dec0914113c629faf07a1f02af9
NoorFatima01 <nfatima101204@gmail.com>
2023-07-09 18:18:37 +0500

    Added comments to clarify the plotting of decision boundary

--- src/util.py
+++ src/util.py
@@ -69,13 +69,29 @@ def plot(x,y,theta,save_path=None,correction=1.0):
     #WHAT DOES LINEWIDTH SPECIFY????
 
 
-    #THIS STEP IS STILL UNCLEAR!!!!
+    #CLARIFICATION FOR THE DECISION BOUNDARY 
+    
+    #Our decision threshold is 0.5. The function h(x) gives 0.5 when theta.T.dot(x) is == 0
+    #So, for our given features and parameters, the decision boundary is when y = 0
+    # ===> theta[0] + theta[1]x1 + theta[2]x2 = 0  where x1 = values on x_axis,   x2 = values on y_axis
+    
+
     # Plot decision boundary (found by solving for theta^T x = 0)
     margin1 = (max(x[:, -2]) - min(x[:, -2]))*0.2
     margin2 = (max(x[:, -1]) - min(x[:, -1]))*0.2
+    #Margin refers to the additional space added around the range of values on a specific axis in a plot
+    
     x1 = np.arange(min(x[:, -2])-margin1, max(x[:, -2])+margin1, 0.01)
+    #This line generates an array of x-axis values (x1) using np.arange(). The arange() function generates values ranging from min(x[:, -2])-margin1 to max(x[:, -2])+margin1 (inclusive) with a step size of 0.01.
+    #This line essentially creates a range of x-axis values that covers the extent of the data points plus the calculated margins.
+
+    #The value of x2 is calculated from the above equation using the x1 array.
     x2 = -(theta[0] / theta[2] * correction + theta[1] / theta[2] * x1)
+    
+    #The decision boundary is then plotted.
     plt.plot(x1, x2, c='red', linewidth=2)
+
+    #.lim() determines the minimum and maximum values for the x-and-y-axis, considering the data points and the calculated margin.
     plt.xlim(x[:, -2].min()-margin1, x[:, -2].max()+margin1)
     plt.ylim(x[:, -1].min()-margin2, x[:, -1].max()+margin2)
     
